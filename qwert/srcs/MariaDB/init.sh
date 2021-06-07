361490982e0575ba52cb00926eb8862ac1743758
Max <30975037+ai-maxim@users.noreply.github.com>
2021-06-07 16:09:22 +0300

    test inception

---
+++ qwert/srcs/MariaDB/init.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+# Configure a wordpress database
+service mysql start
+echo "CREATE DATABASE wordpress;"| mysql -u root --skip-password
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;"| mysql -u root --skip-password
+echo "FLUSH PRIVILEGES;"| mysql -u root --skip-password
+echo "update mysql.user set plugin='' where user='root';"| mysql -u root --skip-password
+service mysql stop
