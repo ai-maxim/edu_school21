dc555ae230dff13772d5c97114d9a678bf92ab85
mix <mix@qdong.42.fr>
2021-06-11 22:04:50 +0300

    rabotushi inception

--- inception/srcs/MariaDB/init.sh
+++ inception/srcs/MariaDB/init.sh
@@ -5,4 +5,6 @@ echo "CREATE DATABASE wordpress;"| mysql -u root --skip-password
 echo "CREATE USER 'mix'@'%' IDENTIFIED BY 'aq';" | mysql -u root --skip-password
 echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'mix'@'%' WITH GRANT OPTION;"| mysql -u root --skip-password
 echo "FLUSH PRIVILEGES;"| mysql -u root --skip-password
+mysql -u root wordpress < /var/www/damp_db.sql
+#cat /var/www/damp_db.sql | mysql -u root --skip-password -p wordpress
 service mysql stop
