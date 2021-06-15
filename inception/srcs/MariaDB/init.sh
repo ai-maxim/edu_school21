ba14c71518a786854b36a0631a2d5084ead45066
Quellon Dong <qdong@il-a1.msk.21-school.ru>
2021-06-15 15:50:59 +0300

    nastroeny peremennye okrugenuy

--- inception/srcs/MariaDB/init.sh
+++ inception/srcs/MariaDB/init.sh
@@ -2,9 +2,8 @@
 # Configure a wordpress database
 service mysql start
 echo "CREATE DATABASE wordpress;"| mysql -u root --skip-password
-echo "CREATE USER 'mix'@'%' IDENTIFIED BY 'aq';" | mysql -u root --skip-password
+echo "CREATE USER 'mix'@'%' IDENTIFIED BY ${WORDPRESS_ADMIN_PASSWORD};" | mysql -u root --skip-password
 echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'mix'@'%' WITH GRANT OPTION;"| mysql -u root --skip-password
 echo "FLUSH PRIVILEGES;"| mysql -u root --skip-password
 mysql -u root wordpress < /var/www/damp_db.sql
-#cat /var/www/damp_db.sql | mysql -u root --skip-password -p wordpress
 service mysql stop
